0c17cca5d46844a681f8fa577af7fd98056ca0b4
hajodomroes <hajo.domroes@gmx.de>
2023-11-14 12:10:14 +0100

    adding dialog

--- meals/src/main/java/hajo/meals/frontend/script.js
+++ meals/src/main/java/hajo/meals/frontend/script.js
@@ -2,3 +2,6 @@ function submitFunction( s ){
     let meal = '{"Gericht", "lvl", "ingrediants", "category"}';
 	alert('Gericht "' + s + '" wurde gespeichert!' + meal);
 }
+function openInputDialog() {
+  document.getElementById("inputDialog").open = true;
+}
